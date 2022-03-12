52adb2bb6faa97e87bc3f81dbac240750460d4d1
Petr Jíša <petr.jisa1406@gmail.com>
2022-03-12 11:30:14 +0100

    Main script

---
+++ CZ_climate.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+from plotmanager import PlotManager
+
+
+@st.cache
+def all_data():
+    return pd.read_csv('Data.csv')
+
+# Veškerá data v proměnné (využití cache pro zrychlení aplikace)
+data = all_data()
+
+# Nadpis a deklarace zdroje dat
+title = 'PROHLÍŽEČ HISTORICKÝCH DAT Z METEOROLOGICKÝCH STANIC ČHMÚ'
+st.markdown(f"<h2 style='text-align: center; color: red'>{title}</h2>", unsafe_allow_html=True)
+st.write('**Zdroj dat: Český hydrometeorologický ústav (ČHMÚ)**')
+
+# Oddělovací čára
+st.markdown('---')
+
+# Hlavní widgety - stanice, veličina, počáteční rok
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    station = st.selectbox('Výběr meteorologické stanice', sorted(list(set(data['Stanice']))))
+
+with col2:
+    quantity = st.selectbox('Měřená veličina', PlotManager.quantities.keys())
+
+with col3:
+    start_year = st.selectbox('Data od roku', (1980, 1975, 1970, 1965, 1961))
+
+# Vedlejší widgety - filtr měsíčních dat, řazení, zobrazení průměru
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    filter = st.selectbox('Filtr', PlotManager.filters)
+
+with col2:
+    sort = st.radio('Řazení', ['chronologické', 'vzestupné', 'sestupné'])
+
+with col3:
+    average = st.radio('Staniční normál', ['1961 - 1990', '1981 - 2010'])
+
+# Oddělovací čára
+st.markdown('---')
+
+# Instance třídy PlotManager podle výběru stanice
+PM = PlotManager(station, data)
+
+# Tisk poznámky, pokud pro danou stanici existuje
+if station in PM.station_remarks.keys():
+    st.write('Poznámka: ', PM.station_remarks[station])
+
+# Grafický výstup
+graphic_result = PM.plot_req(quantity, filter, sort, start_yr=start_year, avg=average)
+if graphic_result == 'Data nejsou k dispozici':
+    st.write('Data pro zobrazení grafu nejsou k dispozici')
+else:
+    st.pyplot(graphic_result)
